f165aced08c62e89da36b3dabbfa88580f79162e
Jonathan Jermstad <jonathan.jermstad@gmail.com>
2024-01-31 17:23:37 -0600

    Created Multiple Loading Options and Formatted the .ipynb file better

--- AERO_401/STK_Sim.py
+++ AERO_401/STK_Sim.py
@@ -5,6 +5,9 @@ from agi.stk12.stkobjects import *
 from agi.stk12.stkutil import *
 from agi.stk12.vgt import *
 from Scripts import *
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
 # if using astrogator uncomment the below
 # from agi.stk12.stkobjects.astrogator
 # if using aviator uncomment the below
@@ -38,23 +41,30 @@ class STK_Simulation:
         print("Loaded Scenario")
         Print_Spacing()
 
-    def Target_Loader(self,Target_Load_Option=0):
+    def Target_Loader(self,Target_Load_Option,Num_Targets):
         print("Loading Targets...")
         Print_Spacing()
-        if Target_Load_Option == 0:
-            self.targets = {}
-            for i in self.root.CurrentScenario.Children.GetElements(AgESTKObjectType.eTarget):
-                i.Unload()
-            for i in range(15):
+        print("tar##\t:\tlon\tlat")
+        self.targets = {}
+        for i in self.root.CurrentScenario.Children.GetElements(AgESTKObjectType.eTarget):
+            i.Unload()
+        for i in range(Num_Targets):
+            if Target_Load_Option == 0:
                 lon = float(decimal.Decimal(random.randrange(-900000, 900000))/10000)
                 lat = float(decimal.Decimal(random.randrange(-900000, 900000))/10000)
-                self.targets[f'Target{i+1}'] = self.root.CurrentScenario.Children.New(AgESTKObjectType.eTarget, f'Target{i+1}')
-                # IAgFacility target: Target Object
-                self.targets[f'Target{i+1}'].Position.AssignGeodetic(lon, lat, 0)  # Latitude, Longitude, Altitude
-                # Set altitude to height of terrain
-                self.targets[f'Target{i+1}'].UseTerrain = True
-                # Set altitude to a distance above the ground
-                self.targets[f'Target{i+1}'].HeightAboveGround = 0   # km
+            else:
+                lon = float(np.linspace(-90,90,Num_Targets)[i])
+                lat = float(np.linspace(-90,90,Num_Targets)[i])
+            
+            print("{:.4f}\t:+t{:.4f}\t|\t{:.4f}".format(i+1,lon,lat))
+
+            self.targets[f'Target{i+1}'] = self.root.CurrentScenario.Children.New(AgESTKObjectType.eTarget, f'Target{i+1}')
+            # IAgFacility target: Target Object
+            self.targets[f'Target{i+1}'].Position.AssignGeodetic(lon, lat, 0)  # Latitude, Longitude, Altitude
+            # Set altitude to height of terrain
+            self.targets[f'Target{i+1}'].UseTerrain = True
+            # Set altitude to a distance above the ground
+            self.targets[f'Target{i+1}'].HeightAboveGround = 0   # km
         print("Loaded Targets")
         Print_Spacing()
             
@@ -62,70 +72,84 @@ class STK_Simulation:
                          inc_range=(45,90),aop_range=(45,90),asc_range=(45,90),loc_range=(45,90)):
         print("Loading Satellites...")
         Print_Spacing()
-        if Satellite_Load_Option == 0:
-            self.satellites = {}
-            for i in self.root.CurrentScenario.Children.GetElements(AgESTKObjectType.eSatellite):
-                i.Unload()
+        print(" sat##\t|\t  per\t\t|\t  ago\t\t|\t  inc\t|\t  aop\t|\t  asc\t|\t  loc")
+        self.satellites = {}
+        for i in self.root.CurrentScenario.Children.GetElements(AgESTKObjectType.eSatellite):
+            i.Unload()
 
-            for i in range(Num_Satellites):
+        for i in range(Num_Satellites):
+            if Satellite_Load_Option == 0:
                 perigee_altitude = Random_Decimal(per_range)
                 apogee_altitude = Random_Decimal(ago_range)
-                
                 inclination = Random_Decimal(inc_range)
                 argument_of_perigee = Random_Decimal(aop_range)
                 ascending_node = Random_Decimal(asc_range)
                 location = Random_Decimal(loc_range)
+            else:
+                perigee_altitude = np.linspace(per_range[0],per_range[1],12)[i]
+                apogee_altitude = np.linspace(ago_range[0],ago_range[1],12)[i]
+                inclination = np.linspace(aop_range[0],aop_range[1],12)[i]
+                argument_of_perigee = np.linspace(aop_range[0],aop_range[1],12)[i]
+                ascending_node = np.linspace(asc_range[0],asc_range[1],12)[i]
+                location = np.linspace(loc_range[0],loc_range[1],12)[i]
 
-                self.satellites[f'Satellite{i+1}'] = self.root.CurrentScenario.Children.New(AgESTKObjectType.eSatellite, f'Satellite{i+1}')
+            print("{:.4f}\t|\t{:.4f}\t|\t{:.4f}\t|\t{:.4f}\t|\t{:.4f}\t|\t{:.4f}\t|\t{:.4f}".format(i+1,perigee_altitude,apogee_altitude,inclination,argument_of_perigee,ascending_node,location))
+            self.satellites[f'Satellite{i+1}'] = self.root.CurrentScenario.Children.New(AgESTKObjectType.eSatellite, f'Satellite{i+1}')
 
-                # IAgSatellite satellite: Satellite object
-                keplerian = self.satellites[f'Satellite{i+1}'].Propagator.InitialState.Representation.ConvertTo(AgEOrbitStateType.eOrbitStateClassical)
-                keplerian.SizeShapeType = AgEClassicalSizeShape.eSizeShapeAltitude
-                keplerian.LocationType = AgEClassicalLocation.eLocationTrueAnomaly
-                keplerian.Orientation.AscNodeType = AgEOrientationAscNode.eAscNodeLAN
+            # IAgSatellite satellite: Satellite object
+            keplerian = self.satellites[f'Satellite{i+1}'].Propagator.InitialState.Representation.ConvertTo(AgEOrbitStateType.eOrbitStateClassical)
+            keplerian.SizeShapeType = AgEClassicalSizeShape.eSizeShapeAltitude
+            keplerian.LocationType = AgEClassicalLocation.eLocationTrueAnomaly
+            keplerian.Orientation.AscNodeType = AgEOrientationAscNode.eAscNodeLAN
 
-                # Assign the perigee and apogee altitude values:
-                keplerian.SizeShape.PerigeeAltitude = perigee_altitude      # km
-                keplerian.SizeShape.ApogeeAltitude = apogee_altitude        # km
+            # Assign the perigee and apogee altitude values:
+            keplerian.SizeShape.PerigeeAltitude = perigee_altitude      # km
+            keplerian.SizeShape.ApogeeAltitude = apogee_altitude        # km
 
-                # Assign the other desired orbital parameters:
-                keplerian.Orientation.Inclination = inclination                 # deg
-                keplerian.Orientation.ArgOfPerigee = argument_of_perigee        # deg
-                keplerian.Orientation.AscNode.Value = ascending_node            # deg
-                keplerian.Location.Value = location                             # deg
+            # Assign the other desired orbital parameters:
+            keplerian.Orientation.Inclination = inclination                 # deg
+            keplerian.Orientation.ArgOfPerigee = argument_of_perigee        # deg
+            keplerian.Orientation.AscNode.Value = ascending_node            # deg
+            keplerian.Location.Value = location                             # deg
 
-                # Apply the changes made to the satellite's state and propagate:
-                self.satellites[f'Satellite{i+1}'].Propagator.InitialState.Representation.Assign(keplerian)
-                self.satellites[f'Satellite{i+1}'].Propagator.Propagate()
+            # Apply the changes made to the satellite's state and propagate:
+            self.satellites[f'Satellite{i+1}'].Propagator.InitialState.Representation.Assign(keplerian)
+            self.satellites[f'Satellite{i+1}'].Propagator.Propagate()
 
-                # IAgSatellite satellite: Satellite object
-                sensor = self.satellites[f'Satellite{i+1}'].Children.New(AgESTKObjectType.eSensor, f'Sensor{i+1}')
-                sensor.CommonTasks.SetPatternSimpleConic(5.0,0.1)
-                attitudePointing = self.satellites[f'Satellite{i+1}'].Attitude.Pointing
-                attitudePointing.UseTargetPointing = True
+            # IAgSatellite satellite: Satellite object
+            sensor = self.satellites[f'Satellite{i+1}'].Children.New(AgESTKObjectType.eSensor, f'Sensor{i+1}')
+            sensor.CommonTasks.SetPatternSimpleConic(5.0,0.1)
+            attitudePointing = self.satellites[f'Satellite{i+1}'].Attitude.Pointing
+            attitudePointing.UseTargetPointing = True
         print("Loaded Satellites")
         Print_Spacing()
 
-        def plot(self):
-            fig = make_subplots(rows=5, cols=3,shared_xaxes=True,shared_yaxes=True)
-            for j in self.targets:
-                targetCoverage = self.targets[j].ObjectCoverage
-                targetCoverage.Assets.RemoveAll()
-                for i in self.satellites:
-                    targetCoverage.Assets.Add(f'Satellite/{i}')
-                targetCoverage.UseObjectTimes = True
-                targetCoverage.Compute()
+    def plot_FOM(self,FOM):
+        fig = make_subplots(rows=5, cols=3,shared_xaxes=True,shared_yaxes=True)
+        for j in self.targets:
+            targetCoverage = self.targets[j].ObjectCoverage
+            targetCoverage.Assets.RemoveAll()
+            for i in self.satellites:
+                targetCoverage.Assets.Add(f'Satellite/{i}')
+            targetCoverage.UseObjectTimes = True
+            targetCoverage.Compute()
 
-                targetCoverageFOM = targetCoverage.FOM
-                targetCoverageFOM.SetDefinitionType(AgEFmDefinitionType.eFmNumberOfAccesses)
-                targetCoverageFOM.Definition.SetComputeType(AgEFmCompute.eTotal)
-                
-                DP = targetCoverage.DataProviders.Item('FOM by Time').Exec(self.scenario.StartTime, self.scenario.StopTime, 15)
-                data = DP.DataSets.ToArray()
+            targetCoverageFOM = targetCoverage.FOM
+            targetCoverageFOM.SetDefinitionType(FOM)
+            targetCoverageFOM.Definition.SetComputeType(AgEFmCompute.eTotal)
+            
+            DP = targetCoverage.DataProviders.Item('FOM by Time').Exec(self.root.CurrentScenario.StartTime, self.root.CurrentScenario.StopTime, 15)
+            data = DP.DataSets.ToArray()
+            if len(data) > 0:
                 data = np.delete(data,2,1)
                 fig.add_trace(
                     go.Scatter(x=data[:,0], y=data[:,1]),
                     row=list(self.targets.keys()).index(j)//3+1, col=list(self.targets.keys()).index(j)%3+1
                 )
-            fig.update_xaxes(showticklabels=False) # hide all the xticks
-            fig.show()
+            else:
+                fig.add_trace(
+                    go.Scatter(x=[0], y=[0]),
+                    row=list(self.targets.keys()).index(j)//3+1, col=list(self.targets.keys()).index(j)%3+1
+                )
+        fig.update_xaxes(showticklabels=False) # hide all the xticks
+        fig.show()
