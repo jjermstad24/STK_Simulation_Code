d968f08c3ed3d2451b1c059b427ab955638fb208
Jermstad <sorryaboutthemess@tamu.edu>
2024-10-04 16:20:29 -0500

    Updated Things

--- STK_Sim.py
+++ STK_Sim.py
@@ -209,8 +209,7 @@ class STK_Simulation:
                     Intervals = access.DataProviders.GetItemByName('AER Data').Group.Item(0).ExecElements(self.root.CurrentScenario.StartTime,
                                                                                                 self.root.CurrentScenario.StopTime,
                                                                                                 self.dt,['Time','Azimuth','Elevation']).Intervals
-                    start_times = []
-                    stop_times = []
+                    times = []
                     bins = []
                     for Int in Intervals:
                         times_az = Int.MultipleThresholdCrossings("Azimuth",az_range)
@@ -222,22 +221,23 @@ class STK_Simulation:
                                         if (any(t[0] <= value <= t[1] for value in t_az[0]) or 
                                             any(t_az[0][0] <= value <= t_az[0][1] for value in t)):
                                             self.Update_Target_Bins(t_az[0][0],i*9+j,tar_num)
-                                            start_times.append(t_az[0][0])
-                                            stop_times.append(t_az[0][1])
-                                            bins.append(i*9+j)
+                                            time_range = [float(i) for i in np.append(np.arange(t_az[0][0],t_az[0][1],self.dt),t_az[0][1])]
+                                            times.extend(time_range)
+                                            bins.extend(len(time_range)*[i*9+j])
                                             
                     access = sat.GetAccessToObject(tar)
-                    res = access.DataProviders.GetItemByName('Sat Angles Data').ExecSingleElementsArray(start_times,['Cross Track','Along Track'])
+                    res = access.DataProviders.GetItemByName('Sat Angles Data').ExecSingleElementsArray(times,['Cross Track','Along Track'])
                     crosstrack = res.GetArray(0)
                     alongtrack = res.GetArray(1)
-                    for b,t,ct,at in zip(bins,start_times,crosstrack,alongtrack):
+                    for b,t,ct,at in zip(bins,times,crosstrack,alongtrack):
                         self.Pre_Planning_Hash_Map[tar_num][b].append([t,ct,at,sat_num])
+                    
                     bar()
                     
         with alive_bar(len(self.targets)*324,force_tty=True,bar='classic',title='- Sorting_Data',length=10) as bar:
             for tar_num in range(len(self.targets)):
                 for bin_num in range(324):
-                    a = np.array(self.Pre_Planning_Hash_Map[tar_num][bin_num]).astype(float)
+                    a = np.array(self.Pre_Planning_Hash_Map[tar_num][bin_num],dtype=float)
                     if len(a) > 0:
                         self.Pre_Planning_Hash_Map[tar_num][bin_num] = a[a[:,0].argsort()]
                     bar()
