ef9df1d93c6ad57ab4b7fbfed013709e398c099f
Cieri <rcieri@tamu.edu>
2024-10-09 18:09:50 -0500

    results notebook

--- Subteam_Directories/OCD/OCD_Runner_PY.py
+++ Subteam_Directories/OCD/OCD_Runner_PY.py
@@ -10,6 +10,8 @@ print("Loaded STK")
 
 n_pop = 10
 n_gen = 5
+n_pop = 5
+n_gen = 1
 n_sats = 8
 
 opt = Optimizer(stk_object,n_pop,n_gen,n_sats)
@@ -23,18 +25,18 @@ print("Beginning Optimization")
 with open(f"../../Pop_Over_Gen/{n_sats}.csv","w") as f:
     hof = opt.run(read=True,enable_print=True,file=f)
 
-# opt.Load_Individual(hof[0])
+opt.Load_Individual(hof[0])
 
-# stk_object.set_sim_time(days=30)
-# stk_object.dt = 60
+stk_object.set_sim_time(days=30)
+stk_object.dt = 60
 
-# for n_targets in [15,34,65,82,109,186,494]:
-#     stk_object.Target_Loader(f"../../Input_Files/Target_Packages/Targets_{n_targets}.txt")
-#     stk_object.Generate_Pre_Planning_Data()
-#     stk_object.Plan(1,20)
+for n_targets in [15,34,65,82,109,186,494]:
+    stk_object.Target_Loader(f"../../Input_Files/Target_Packages/Targets_{n_targets}.txt")
+    stk_object.Generate_Pre_Planning_Data()
+    stk_object.Plan(1,20)
 
-#     data_comparison = {}
-#     data_comparison["Unplanned (%)"] = [np.count_nonzero(stk_object.target_bins[tar_num])/324*100 for tar_num in range(len(stk_object.targets))]
-#     data_comparison["Unplanned (Time)"] = [np.max(stk_object.target_times[tar_num])/86400 for tar_num in range(len(stk_object.targets))]
-#     pd.DataFrame(data_comparison).to_csv(f"../../Optimization_Results/{n_sats}_{n_targets}.csv",index=False)
-#     data_comparison = pd.DataFrame(data_comparison)
+    data_comparison = {}
+    data_comparison["Unplanned (%)"] = [np.count_nonzero(stk_object.target_bins[tar_num])/324*100 for tar_num in range(len(stk_object.targets))]
+    data_comparison["Unplanned (Time)"] = [np.max(stk_object.target_times[tar_num])/86400 for tar_num in range(len(stk_object.targets))]
+    pd.DataFrame(data_comparison).to_csv(f"../../Optimization_Results/{n_sats}_{n_targets}.csv",index=False)
+    data_comparison = pd.DataFrame(data_comparison)
