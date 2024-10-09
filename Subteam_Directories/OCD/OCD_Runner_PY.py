ebb6256145928ec7f04edb414902033c8dbe6e7d
Cieri <rcieri@tamu.edu>
2024-10-09 13:19:23 -0500

    fixed bugs

--- Subteam_Directories/OCD/OCD_Runner_PY.py
+++ Subteam_Directories/OCD/OCD_Runner_PY.py
@@ -14,7 +14,7 @@ n_sats = 8
 
 opt = Optimizer(stk_object,n_pop,n_gen,n_sats)
 
-stk_object.set_sim_time(days=15)
+stk_object.set_sim_time(days=30)
 stk_object.Target_Loader("../../Input_Files/Target_Packages/Targets_65.txt")
 
 pd.read_csv(f"../../Input_Files/Constellations/{n_sats}.txt").to_csv(f"../../Input_Files/Satellites_File_{n_sats}.txt",index=False)
@@ -38,6 +38,7 @@ for n_targets in [15,34,65,82,109,186,494]:
     data_comparison["Planned (%)"] = [len(np.unique(stk_object.Planned_Data[stk_object.Planned_Data['Target'].values==tar_num]['Bin Number'].values))/324*100 for tar_num in range(len(stk_object.targets))]
     data_comparison["Planned (Time)"] = [max(stk_object.Planned_Data[stk_object.Planned_Data['Target'].values==tar_num]['Time'].values)/86400 for tar_num in range(len(stk_object.targets))]
     pd.DataFrame(data_comparison).to_csv(f"../../Optimization_Results/{n_sats}_{n_targets}.csv",index=False)
+    data_comparison = pd.DataFrame(data_comparison)
 
     discord_message = f"```\n{round(pd.concat([data_comparison.describe().iloc[1:3], data_comparison.describe().iloc[[-1]]]),4).to_string()}\n```"
-    send_message_to_discord(channel_id=1203813613903675502,message=discord_message)
+    send_message_to_discord(channel_id=1203813613903675502,message=discord_message) # , bot_token=)
