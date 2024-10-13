42109b20fafdccb1e8b493e1defcc22803815839
Cieri <rcieri@tamu.edu>
2024-10-13 18:11:04 -0500

    added 3_109 and 3_186

--- Scripts.py
+++ Scripts.py
@@ -139,7 +139,7 @@ class Optimizer:
             file.write("Gen,Pop,Alt,Inc,Initial_Raan,Delta_Raan,Num_Planes,Avg_Percentage,Std_Percentage,Avg_Time,Std_Time,\n")
             for i in range(self.n_pop):
                 file.write(f"{g},{i},")
-                for idx in range(4):
+                for idx in range(5):
                     file.write(f"{pop[i][idx]},")
                 for fit in pop[i].fitness.getValues():
                     file.write(f"{fit},")
