005043ca3292f940f2bcdf6ef4c5bdefc1e7aa92
Cieri <rcieri@tamu.edu>
2024-10-21 18:09:00 -0500

    fixed runner

--- Subteam_Directories/OCD/OCD_Runner_PY.py
+++ Subteam_Directories/OCD/OCD_Runner_PY.py
@@ -7,8 +7,8 @@ from STK_Sim import *
 Filename = 'AERO_402_Further_Assessment'
 
 stk_object = STK_Simulation(False,Filename)
-stk_object.set_sim_time(days=3)
-stk_object.Target_Loader("../../Input_Files/Target_Packages/Targets_15.txt")
+stk_object.set_sim_time(days=30)
+stk_object.Target_Loader("../../Input_Files/Target_Packages/Targets_65.txt")
 
 
 
@@ -20,7 +20,7 @@ stk_object.Target_Loader("../../Input_Files/Target_Packages/Targets_15.txt")
 per_weight = .5
 time_weight = .25
 cost_weight = 1-time_weight-per_weight
-opt = Optimizer(stk_object,n_pop=2,n_gen=1,run_num=0,weights=(per_weight,-time_weight,-cost_weight))
+opt = Optimizer(stk_object,n_pop=10,n_gen=10,run_num=0,weights=(per_weight,-time_weight,-cost_weight))
 print("Beginning Optimization")
 opt.run(read=False,enable_print=True)
 
